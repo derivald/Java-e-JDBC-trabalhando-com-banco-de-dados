5de3f1bd033b34eed84660360c5ee9e18da86fc2
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-01-03 00:44:32 -0300

    Estabilidade com pool de conexoes

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,30 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
-	Connection recuperarConexao() throws SQLException {
-		return  DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "derivaldo", "1565Celia@");
+	public DataSource dataSource;
+	
+	public  ConnectionFactory(){
+			ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+			comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+			comboPooledDataSource.setUser("derivaldo");
+			comboPooledDataSource.setPassword("1565Celia@");
+			
+			comboPooledDataSource.setMaxPoolSize(15);
+			
+			this.dataSource = comboPooledDataSource;
+	}
+	
+	public Connection recuperarConexao() throws SQLException {
+		
+		//"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "derivaldo", "1565Celia@"
+		
+		return  this.dataSource.getConnection();
 		
 	}
 
