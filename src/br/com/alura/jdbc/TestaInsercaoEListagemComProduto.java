afdc75b5cd67f2ff34d8dea69f62c6b058f58058
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-01-04 15:14:38 -0300

    Evitando queriesN+1

--- src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
@@ -10,17 +10,16 @@ import br.com.alura.jdbc.modelo.Produto;
 public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
-		
-		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			ProdutoDAO produtoDao = new ProdutoDAO(connection);
 			produtoDao.salvar(comoda);
-			
+
 			List<Produto> listaDeProdutos = produtoDao.listar();
 			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
-		
 
 	}
 
